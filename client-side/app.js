f603268943e21fb98144c9d985b19305bd662683
zakattack9 <zakattack26111@gmail.com>
2017-11-01 10:58:52 -1000

    got Lambda working

---
+++ client-side/app.js
@@ -0,0 +1,3 @@
+// endpoint for lambda  https://lfgw8y047g.execute-api.us-west-2.amazonaws.com/dev/users/photos
+
+$.get(" https://lfgw8y047g.execute-api.us-west-2.amazonaws.com/dev/users/photos", (data,status) => return data, JSON);
--- handler.js
+++ handler.js
@@ -3,18 +3,22 @@
 module.exports.getPhotos = (event, context, callback) => {
   const AWS = require('aws-sdk');
   const s3 = new AWS.S3();
-  const params = {Bucket: 'photo-bucket-tmp-prjct'};
-  s3.listObjects() //add params, check docs
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
+  const params = {
+    Bucket: 'photo-bucket-tmp-prjct'
   };
 
-  callback(null, response);
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+  s3.listObjects(params, (err, data) => {
+    const response = {
+      statusCode: 200,
+      headers: {
+       "Access-Control-Allow-Origin": "*",
+       "Access-Control-Allow-Credentials": true
+      },
+      body: {
+        message: JSON.stringify(data)
+        //input: event,
+      },
+    };
+    callback(null, response);
+  });
 };
