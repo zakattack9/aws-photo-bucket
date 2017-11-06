ce373b5b2461fd468a5a8cc2ed5cefa93b17359f
zakattack9 <zakattack26111@gmail.com>
2017-11-06 10:45:28 -1000

    add pictures

--- client-side/app.js
+++ client-side/app.js
@@ -1,13 +1,13 @@
-// endpoint for lambda  https://lfgw8y047g.execute-api.us-west-2.amazonaws.com/dev/users/photos
+// endpoint for lambda https://1zguq18qmj.execute-api.us-west-2.amazonaws.com/dev/users/photos
+$('button').click(function(){
+	$.ajax({
+		url: "https://1zguq18qmj.execute-api.us-west-2.amazonaws.com/dev/users/photos",
+		type: 'GET',
+		success: data => data.message.Contents.map(currVal => generateImg(`https://s3-us-west-2.amazonaws.com/photo-bucket-tmp-prjct/${currVal.Key}`)),
+		dataType: 'JSON'
+	})
+});
 
-/*$('button').click( function(){
-	$.get(
-		"https://6yvtnxwo7d.execute-api.us-west-2.amazonaws.com/dev/users/photos",
-		(data,status) => {
-			return data
-		},
-		JSON
-	)
-});*/
-
-$.get("https://6yvtnxwo7d.execute-api.us-west-2.amazonaws.com/dev/users/photos");
+let generateImg = image => {
+	$("#pictures").append($(`<img class="image" src="${image}"/>`).hide().fadeIn(500));
+}
